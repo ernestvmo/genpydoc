ee99478e99d0b8ac81b1cb76e98011b51286d725
Ernest Vanmosuinck <ernest.vanmo@gmail.com>
2026-01-23 17:58:17 +0100

    run analyzer on diffed files rather than on nodes covered

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,3 +1,6 @@
+import os.path
+import sys
+
 from git import Diff, Repo
 
 from analyzer.git_utils import process_git_diff
@@ -17,12 +20,17 @@ class Analyzer:
         self.nodes = nodes
         self.lines = {}
         self.__add_all()
-        self._renamed_map = self.__build_renamed_map()
+        self._diffed_map = self.__build_diffed_map()
+
+        if not self._diffed_map or all(
+            k not in self.covered_nodes.keys() for k in self._diffed_map.keys()
+        ):
+            self.__stop_early()
 
     def __add_all(self):
         self.repo.git.add(all=True)
 
-    def __build_renamed_map(self):
+    def __build_diffed_map(self):
         def _reverse_mapping(ct: str | None) -> str:
             mapping = {"D": "A", "A": "D"}
             if ct not in mapping:
@@ -30,23 +38,31 @@ class Analyzer:
             return mapping[ct]
 
         d = self.repo.index.diff("HEAD")
-        return {c.a_path: _reverse_mapping(c.change_type) for c in d}
+        return {
+            os.path.join(self.root, c.a_path): _reverse_mapping(c.change_type)
+            for c in d
+        }
+
+    @staticmethod
+    def __stop_early():
+        """Ends the program early"""
+        sys.exit()
 
     @staticmethod
     def _process_diff(diff: Diff):
         return process_git_diff(diff)
 
     def _extract_lines(self):
-        """Comment"""
         lines_for_evaluation = {}
-        for k in self.covered_nodes.keys():
+        for k in self._diffed_map.keys():
+            # for k in self.covered_nodes.keys():
             diff = self.repo.index.diff("HEAD", paths=k, create_patch=True)
             if len(diff) > 1:
                 raise ValueError
             if len(diff):
                 diff = diff[0]
 
-            if self._renamed_map[k.removeprefix(self.root + "/")] == "A":
+            if self._diffed_map.get(k, "A") == "A":
                 lines_for_evaluation[k] = {"*"}
             else:
                 lines = self._match_lines_to_ast(k, self._process_diff(diff))
@@ -54,6 +70,7 @@ class Analyzer:
         return lines_for_evaluation
 
     def _match_lines_to_ast(self, k: str, lines: set[int]) -> set[str]:
+        print(lines)
         definitions = set()
         for line in lines:
             traversed_nodes: list[CovNode] = []
