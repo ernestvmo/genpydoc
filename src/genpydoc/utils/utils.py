120dadadda5e610e73197898cfe9863c4ed21592
Ernest Vanmosuinck <ernest.vanmo@gmail.com>
2026-02-13 14:52:09 +0100

    fix: PosixPath correct suffix call

--- src/genpydoc/utils/utils.py
+++ src/genpydoc/utils/utils.py
@@ -42,7 +42,7 @@ def read_config_file(
         if value is None:
             return None
 
-    if value.endswith(".toml"):
+    if value.suffix == ".toml":
         try:
             config = parse_pyproject_toml(value)
         except (tomllib.TOMLDecodeError, OSError) as err:
